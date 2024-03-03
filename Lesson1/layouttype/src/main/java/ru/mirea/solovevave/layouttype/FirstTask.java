9dea2408c7e4703a2502c243d10ee022f0b7afb9
ecrsty <viktoriasoloveva05@gmail.com>
2024-03-03 23:25:11 +0300

    first practice

---
+++ Lesson1/layouttype/src/main/java/ru/mirea/solovevave/layouttype/FirstTask.java
@@ -0,0 +1,14 @@
+package ru.mirea.solovevave.layouttype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_task);
+    }
+}
